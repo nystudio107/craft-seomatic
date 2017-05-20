1644f4bbec8eb5efb5224c6c79e817945dea2924
Andrew Welch <andrew@nystudio107.com>
2017-05-20 02:08:25 -0400

    * Initial webpack config

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+var path = require('path')
+var webpack = require('webpack')
+
+const assetBundleRoot = './src/assetbundles/seomatic/dist';
+
+module.exports = {
+    entry: {
+        'vendor': ['vue', 'axios'],
+    },
+    output: {
+        path: path.resolve(__dirname, assetBundleRoot + '/js'),
+        publicPath: assetBundleRoot,
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {}
+                    // other vue-loader options go here
+                }
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            names: ['vendor']
+        })
+    ],
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.common.js'
+        }
+    },
+    devServer: {
+        historyApiFallback: true,
+        noInfo: true
+    },
+    performance: {
+        hints: false
+    },
+    devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.devtool = '#source-map'
+    // http://vue-loader.vuejs.org/en/workflow/production.html
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        })
+    ])
+}
