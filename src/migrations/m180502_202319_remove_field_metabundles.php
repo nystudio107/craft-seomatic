caef653db56947f4a03b14d919ae4fa815d05b6f
Andrew Welch <andrew@nystudio107.com>
2018-05-02 16:49:39 -0400

    Added a migration to remove any errant `seomatic_metabundles` rows that have `sourceBundleType` set to `field`
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

---
+++ src/migrations/m180502_202319_remove_field_metabundles.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace nystudio107\seomatic\migrations;
+
+use Craft;
+use craft\db\Migration;
+
+/**
+ * m180502_202319_remove_field_metabundles migration.
+ */
+class m180502_202319_remove_field_metabundles extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $this->delete('{{%seomatic_metabundles}}', ['sourceBundleType' => 'field']);
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m180502_202319_remove_field_metabundles cannot be reverted.\n";
+        return false;
+    }
+}
