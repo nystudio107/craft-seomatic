89591b0696ba0a3b50df0299daee139705c2231d
Andrew Welch <andrew@nystudio107.com>
2018-06-18 02:16:46 -0400

    Fixed an issue introduced in Craft CMS 3.0.10 that would cause JSON-LD to be not fully rendered
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/models/MetaBundle.php
+++ src/models/MetaBundle.php
@@ -172,7 +172,8 @@ class MetaBundle extends FluentModel
         // Create our variable so that meta containers can be parsed based on dynamic values
         // Make sure Twig is loaded and instantiated first by priming the pump
         MetaValueHelper::parseString('{prime}');
-        $oldSeomaticVariable = Seomatic::$seomaticVariable;
+        $oldMeta = Seomatic::$seomaticVariable->meta;
+        $oldSite = Seomatic::$seomaticVariable->site;
         $oldLoadingContainers = Seomatic::$loadingContainers;
         $oldPreviewingMetaContainers = Seomatic::$previewingMetaContainers;
         Seomatic::$loadingContainers = false;
@@ -217,7 +218,8 @@ class MetaBundle extends FluentModel
         // Restore the $seomaticVariable
         Seomatic::$loadingContainers = $oldLoadingContainers;
         Seomatic::$previewingMetaContainers = $oldPreviewingMetaContainers;
-        Seomatic::$seomaticVariable = $oldSeomaticVariable;
+        Seomatic::$seomaticVariable->meta = $oldMeta;
+        Seomatic::$seomaticVariable->site = $oldSite;
         MetaValueHelper::cache();
     }
 
