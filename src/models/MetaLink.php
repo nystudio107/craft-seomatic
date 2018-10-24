182279670b0591bbfabd2a07cd3822826f33a8d1
Tim Kelty <tim@fusionary.com>
2018-10-24 12:38:19 -0400

    Fix nest meta

--- src/models/MetaLink.php
+++ src/models/MetaLink.php
@@ -208,7 +208,7 @@ class MetaLink extends MetaItem
         foreach ($configs as $config) {
             if ($this->prepForRender($config)) {
                 ksort($config);
-                $html = Html::tag('link', '', $config);
+                $html .= Html::tag('link', '', $config);
             }
         }
 
@@ -226,7 +226,7 @@ class MetaLink extends MetaItem
         foreach ($configs as $config) {
             if ($this->prepForRender($config)) {
                 ksort($config);
-                $attributes = array_merge($attributes, $config);
+                $attributes[] = $config;
             }
         }
 
