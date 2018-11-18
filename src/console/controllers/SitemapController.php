eb5dbc0880c4e73757e10dd5366f44ea5687f492
Andrew Welch <andrew@nystudio107.com>
2018-11-18 15:28:06 -0500

    Added a console command `./craft seomatic/sitemap/generate` to generate sitemaps via the CLI
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/console/controllers/SitemapController.php
+++ src/console/controllers/SitemapController.php
@@ -9,6 +9,7 @@
 
 namespace nystudio107\seomatic\console\controllers;
 
+use nystudio107\seomatic\models\MetaBundle;
 use nystudio107\seomatic\Seomatic;
 
 use Craft;
@@ -37,11 +38,6 @@ class SitemapController extends Controller
      */
     public $siteId;
 
-    /**
-     * @var null|string
-     */
-    public $type;
-
     // Protected Properties
     // =========================================================================
 
@@ -64,18 +60,52 @@ class SitemapController extends Controller
         return [
             'handle',
             'siteId',
-            'type',
         ];
     }
 
     /**
-     * Generate a sitemap. You can pass in the following parameters:
-     * --handle - the handle to the section
+     * Generate a sitemap. You can pass in a --handle and/or a --siteId
      */
     public function actionGenerate()
     {
         echo 'Generating sitemap'.PHP_EOL;
-        Seomatic::$plugin->sitemaps->invalidateSitemapCache($this->handle, $this->siteId, $this->type, true);
-        Craft::$app->getQueue()->run();
+        if ($this->siteId !== null) {
+            $siteIds[] = $this->siteId;
+        } else {
+            $siteIds = Craft::$app->getSites()->getAllSiteIds();
+            if (!\is_array($siteIds)) {
+                $siteIds = [$siteIds];
+            }
+        }
+        foreach ($siteIds as $siteId) {
+            $metaBundles = Seomatic::$plugin->metaBundles->getContentMetaBundlesForSiteId($siteId);
+            Seomatic::$plugin->metaBundles->pruneVestigialMetaBundles($metaBundles);
+            /** @var MetaBundle $metaBundle */
+            foreach ($metaBundles as $metaBundle) {
+                $process = false;
+                if ($this->handle === null || $this->handle === $metaBundle->sourceHandle) {
+                    $process = true;
+                }
+                if ($metaBundle->metaSitemapVars->sitemapUrls && $process) {
+                    echo 'Generating sitemap for '
+                        .$metaBundle->sourceType
+                        .' '
+                        .$metaBundle->sourceName
+                        .', siteId '
+                        .$siteId;
+                    Seomatic::$plugin->sitemaps->invalidateSitemapCache(
+                        $metaBundle->sourceHandle,
+                        $siteId,
+                        $metaBundle->sourceBundleType,
+                        true
+                    );
+                    Craft::$app->getQueue()->run();
+                    echo '---'.PHP_EOL;
+                }
+            }
+        }
     }
+
+    // Protected Methods
+    // =========================================================================
 }
