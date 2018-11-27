85620a2df64239b8f79aa9dd23551a87795dfa75
Andrew Welch <andrew@nystudio107.com>
2018-11-27 17:33:30 -0500

    Added a level 2 cache on the controller-based API requests for meta containers to improve "headless" performance
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/controllers/MetaContainerController.php
+++ src/controllers/MetaContainerController.php
@@ -17,8 +17,10 @@ use nystudio107\seomatic\models\MetaTitleContainer;
 use nystudio107\seomatic\models\MetaTagContainer;
 
 use Craft;
+use craft\base\Element;
 use craft\web\Controller;
 
+use yii\caching\TagDependency;
 use yii\web\Response;
 use yii\web\JsonResponseFormatter;
 
@@ -29,7 +31,12 @@ use yii\web\JsonResponseFormatter;
  */
 class MetaContainerController extends Controller
 {
-    // Properties
+    // Constants
+    // =========================================================================
+
+    const CACHE_KEY = 'seomatic_metacontroller_';
+
+    // Protected Properties
     // =========================================================================
 
     /**
@@ -209,21 +216,67 @@ class MetaContainerController extends Controller
         // Normalize the incoming URI to account for `__home__`
         $uri = ($uri === '__home__') ? '' : $uri;
 
-        // Load the meta containers and parse our globals
-        Seomatic::$plugin->metaContainers->previewMetaContainers($uri, $siteId, true);
-
-        // Iterate through the desired $containerKeys
-        foreach ($containerKeys as $containerKey) {
-            if ($asArray) {
-                $result[$containerKey] = Seomatic::$plugin->metaContainers->renderContainersArrayByType(
-                    $containerKey
-                );
-            } else {
-                $result[$containerKey] = Seomatic::$plugin->metaContainers->renderContainersByType(
-                    $containerKey
-                );
-            }
+        // Determine the siteId
+        if ($siteId === null) {
+            $siteId = Craft::$app->getSites()->currentSite->id
+                ?? Craft::$app->getSites()->primarySite->id
+                ?? 1;
         }
+        $uri = trim($uri, '/');
+
+        // Add a caching layer on top of controller requests
+        $sourceId = '';
+        /** @var Element $element */
+        $element = Craft::$app->getElements()->getElementByUri($uri, $siteId, false);
+        if ($element !== null) {
+            list($sourceId, $sourceBundleType, $sourceHandle, $sourceSiteId)
+                = Seomatic::$plugin->metaBundles->getMetaSourceFromElement($element);
+        }
+        $metaContainers = Seomatic::$plugin->metaContainers;
+        // Get our cache key
+        $asArrayKey = $asArray ? 'true' : 'false';
+        $cacheKey = $uri.$siteId.implode($containerKeys).$asArrayKey;
+        // Load the meta containers
+        $dependency = new TagDependency([
+            'tags' => [
+                $metaContainers::GLOBAL_METACONTAINER_CACHE_TAG,
+                $metaContainers::METACONTAINER_CACHE_TAG.$sourceId,
+                $metaContainers::METACONTAINER_CACHE_TAG.$uri.$siteId,
+                $metaContainers::METACONTAINER_CACHE_TAG.$cacheKey,
+            ],
+        ]);
+
+        $cache = Craft::$app->getCache();
+        $result = $cache->getOrSet(
+            $this::CACHE_KEY.$cacheKey,
+            function () use ($uri, $siteId, $containerKeys, $asArray) {
+                $result = [];
+                Craft::info(
+                    'Meta controller container cache miss: '.$uri.'/'.$siteId,
+                    __METHOD__
+                );
+                // Load the meta containers and parse our globals
+                Seomatic::$plugin->metaContainers->previewMetaContainers($uri, $siteId, true);
+
+                // Iterate through the desired $containerKeys
+                foreach ($containerKeys as $containerKey) {
+                    if ($asArray) {
+                        $result[$containerKey] = Seomatic::$plugin->metaContainers->renderContainersArrayByType(
+                            $containerKey
+                        );
+                    } else {
+                        $result[$containerKey] = Seomatic::$plugin->metaContainers->renderContainersByType(
+                            $containerKey
+                        );
+                    }
+                }
+
+                return $result;
+            },
+            Seomatic::$cacheDuration,
+            $dependency
+        );
+
         // use "pretty" output in debug mode
         Craft::$app->response->formatters[Response::FORMAT_JSON] = [
             'class' => JsonResponseFormatter::class,
