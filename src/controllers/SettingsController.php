dab351882036a01ebb22e1ee153246c68f3ee81b
Andrew Welch <andrew@nystudio107.com>
2018-12-18 16:51:55 -0500

    Breadcrumbs in the CP now maintain the selected site
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/controllers/SettingsController.php
+++ src/controllers/SettingsController.php
@@ -128,6 +128,7 @@ class SettingsController extends Controller
         $variables['pluginName'] = Seomatic::$settings->pluginName;
         $variables['title'] = $templateTitle;
         $variables['docTitle'] = "{$pluginName} - {$templateTitle}";
+        $siteHandleUri = Craft::$app->isMultiSite ? '/'.$siteHandle : '';
         $variables['crumbs'] = [
             [
                 'label' => $pluginName,
@@ -135,7 +136,7 @@ class SettingsController extends Controller
             ],
             [
                 'label' => $templateTitle,
-                'url' => UrlHelper::cpUrl('seomatic/dashboard'),
+                'url' => UrlHelper::cpUrl('seomatic/dashboard'.$siteHandleUri),
             ],
         ];
         $variables['selectedSubnavItem'] = 'dashboard';
@@ -227,6 +228,7 @@ class SettingsController extends Controller
         $variables['title'] = $templateTitle;
         $variables['subSectionTitle'] = $subSectionTitle;
         $variables['docTitle'] = "{$pluginName} - {$templateTitle} - {$subSectionTitle}";
+        $siteHandleUri = Craft::$app->isMultiSite ? '/'.$siteHandle : '';
         $variables['crumbs'] = [
             [
                 'label' => $pluginName,
@@ -234,11 +236,11 @@ class SettingsController extends Controller
             ],
             [
                 'label' => $templateTitle,
-                'url' => UrlHelper::cpUrl('seomatic/global'),
+                'url' => UrlHelper::cpUrl('seomatic/global/general'.$siteHandleUri),
             ],
             [
                 'label' => $subSectionTitle,
-                'url' => UrlHelper::cpUrl('seomatic/global/'.$subSection),
+                'url' => UrlHelper::cpUrl('seomatic/global/'.$subSection.$siteHandleUri),
             ],
         ];
         $variables['selectedSubnavItem'] = 'global';
@@ -375,6 +377,7 @@ class SettingsController extends Controller
         $variables['pluginName'] = Seomatic::$settings->pluginName;
         $variables['title'] = $templateTitle;
         $variables['docTitle'] = "{$pluginName} - {$templateTitle}";
+        $siteHandleUri = Craft::$app->isMultiSite ? '/'.$siteHandle : '';
         $variables['crumbs'] = [
             [
                 'label' => $pluginName,
@@ -382,7 +385,7 @@ class SettingsController extends Controller
             ],
             [
                 'label' => $templateTitle,
-                'url' => UrlHelper::cpUrl('seomatic/content'),
+                'url' => UrlHelper::cpUrl('seomatic/content'.$siteHandleUri),
             ],
         ];
         $this->setMultiSiteVariables($siteHandle, $siteId, $variables);
@@ -461,6 +464,7 @@ class SettingsController extends Controller
         $variables['title'] = $templateTitle;
         $variables['subSectionTitle'] = $subSectionTitle;
         $variables['docTitle'] = "{$pluginName} - Content SEO - {$templateTitle} - {$subSectionTitle}";
+        $siteHandleUri = Craft::$app->isMultiSite ? '/'.$siteHandle : '';
         $variables['crumbs'] = [
             [
                 'label' => $pluginName,
@@ -468,7 +472,7 @@ class SettingsController extends Controller
             ],
             [
                 'label' => 'Content SEO',
-                'url' => UrlHelper::cpUrl('seomatic/content'),
+                'url' => UrlHelper::cpUrl('seomatic/content'.$siteHandleUri),
             ],
             [
                 'label' => $metaBundle->sourceName.' · '.$subSectionTitle,
@@ -612,6 +616,7 @@ class SettingsController extends Controller
         $variables['title'] = $templateTitle;
         $variables['subSectionTitle'] = $subSectionTitle;
         $variables['docTitle'] = "{$pluginName} - {$templateTitle} - {$subSectionTitle}";
+        $siteHandleUri = Craft::$app->isMultiSite ? '/'.$siteHandle : '';
         $variables['crumbs'] = [
             [
                 'label' => $pluginName,
@@ -619,11 +624,11 @@ class SettingsController extends Controller
             ],
             [
                 'label' => $templateTitle,
-                'url' => UrlHelper::cpUrl('seomatic/site'),
+                'url' => UrlHelper::cpUrl('seomatic/site/identity'.$siteHandleUri),
             ],
             [
                 'label' => $subSectionTitle,
-                'url' => UrlHelper::cpUrl('seomatic/site/'.$subSection),
+                'url' => UrlHelper::cpUrl('seomatic/site/'.$subSection.$siteHandleUri),
             ],
         ];
         $variables['selectedSubnavItem'] = 'site';
@@ -804,6 +809,7 @@ class SettingsController extends Controller
         $variables['title'] = $templateTitle;
         $variables['subSectionTitle'] = $subSectionTitle;
         $variables['docTitle'] = "{$pluginName} - {$templateTitle} - {$subSectionTitle}";
+        $siteHandleUri = Craft::$app->isMultiSite ? '/'.$siteHandle : '';
         $variables['crumbs'] = [
             [
                 'label' => $pluginName,
@@ -815,7 +821,7 @@ class SettingsController extends Controller
             ],
             [
                 'label' => $subSectionTitle,
-                'url' => UrlHelper::cpUrl('seomatic/tracking/'.$subSection),
+                'url' => UrlHelper::cpUrl('seomatic/tracking/'.$subSection.$siteHandleUri),
             ],
         ];
         $variables['selectedSubnavItem'] = 'tracking';
