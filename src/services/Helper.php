8034cf088c9e7a261cc4cff2449c7bf453cf57b9
Andrew Welch <andrew@nystudio107.com>
2019-02-06 20:47:34 -0500

    Add Craft 3.1 version check
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/services/Helper.php
+++ src/services/Helper.php
@@ -389,4 +389,15 @@ class Helper extends Component
     ): string {
         return ImageTransformHelper::socialTransformHeight($asset, $transformName, $siteId, $transformMode);
     }
+
+    /**
+     * Return whether we are running Craft 3.1 or later
+     *
+     * @return bool
+     */
+    public function craft31(): bool
+    {
+        return Seomatic::$craft31;
+    }
+
 }
