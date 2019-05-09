cf282b70e23c1903d3244abc584d8e54ac2dcb65
Andrew Welch <andrew@nystudio107.com>
2019-05-09 18:55:04 -0400

    Refactor CraftQL support
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/helpers/CraftQLSchemaHelper.php
+++ src/helpers/CraftQLSchemaHelper.php
@@ -1,8 +1,19 @@
 <?php
+/**
+ * SEOmatic plugin for Craft CMS 3.x
+ *
+ * @link      https://nystudio107.com/
+ * @copyright Copyright (c) 2019 nystudio107
+ * @license   https://nystudio107.com/license
+ */
 
 namespace nystudio107\seomatic\helpers;
 
-use nystudio107\seomatic\Seomatic;
+use Craft;
+use craft\base\Element;
+use craft\elements\Entry;
+
+use nystudio107\seomatic\helpers\Container as ContainerHelper;
 use nystudio107\seomatic\models\MetaJsonLdContainer;
 use nystudio107\seomatic\models\MetaLinkContainer;
 use nystudio107\seomatic\models\MetaScriptContainer;
@@ -11,11 +22,11 @@ use nystudio107\seomatic\models\MetaTagContainer;
 
 use markhuot\CraftQL\Events\AlterSchemaFields;
 
-use Craft;
-use craft\base\Element;
-
-use yii\caching\TagDependency;
-
+/**
+ * @author    nystudio107
+ * @package   Seomatic
+ * @since     3.2.0
+ */
 class CraftQLSchemaHelper
 {
     // Constants
@@ -23,146 +34,90 @@ class CraftQLSchemaHelper
 
     const CACHE_KEY = 'seomatic_metacontroller_';
 
-    public function handle(AlterSchemaFields $event)
-    {
-        $seoMaticField = $event->schema->createObjectType('SEOMaticData');
-
-        $seoMaticField->addStringField('metaTitleContainer')->resolve(function (Element $element) {
-            // $root contains the data returned by the field below
-            $result = $this->getContainerArrays(
-                [
-                    MetaTitleContainer::CONTAINER_TYPE,
-                ],
-                $element
-            );
-            return $result['MetaTitleContainer'];
-        });
-
-        $seoMaticField->addStringField('metaTagContainer')->resolve(function (Element $element) {
-            // $root contains the data returned by the field below
-            $result = $this->getContainerArrays(
-                [
-                    MetaTagContainer::CONTAINER_TYPE,
-                ],
-                $element
-            );
-            return $result['MetaTagContainer'];
-        });
-
-        $seoMaticField->addStringField('metaLinkContainer')->resolve(function (Element $element) {
-            // $root contains the data returned by the field below
-            $result = $this->getContainerArrays(
-                [
-                    MetaLinkContainer::CONTAINER_TYPE,
-                ],
-                $element
-            );
-            return $result['MetaLinkContainer'];
-        });
-
-        $seoMaticField->addStringField('metaScriptContainer')->resolve(function (Element $element) {
-            // $root contains the data returned by the field below
-            $result = $this->getContainerArrays(
-                [
-                    MetaScriptContainer::CONTAINER_TYPE,
-                ],
-                $element
-            );
-            return $result['MetaScriptContainer'];
-        });
-
-        $seoMaticField->addStringField('metaJsonLdContainer')->resolve(function (Element $element) {
-            // $root contains the data returned by the field below
-            $result = $this->getContainerArrays(
-                [
-                    MetaJsonLdContainer::CONTAINER_TYPE,
-                ],
-                $element
-            );
-            return $result['MetaJsonLdContainer'];
-        });
-
-        $event->schema->addField('seoMatic')
-            ->type($seoMaticField)
-            ->resolve(function (Element $root) {
-                // Return the root, which is the requested Entry for usage in the fields.
-                return $root;
-            });
-    }
-
-    // Protected Methods
+    // Public Methods
     // =========================================================================
 
-    protected function getContainerArrays(
-        array $containerKeys,
-        Element $element,
-        bool $asArray = false
-    ): array {
-        $result = [];
+    public static function handle(AlterSchemaFields $event)
+    {
+        // Create the root object
+        $seomaticField = $event->schema->createObjectType('seomaticData');
+        // Add metaTitleContainer
+        $seomaticField->addStringField('metaTitleContainer')->resolve(function (Element $element) {
+            // $root contains the data returned by the field below
+            $result = ContainerHelper::getContainerArrays(
+                [MetaTitleContainer::CONTAINER_TYPE],
+                $element->uri,
+                $element->siteId,
+                false
+            );
 
-        // Normalize the incoming URI to account for `__home__`
-        $uri = ($element->uri === '__home__') ? '' : $element->uri;
-        $siteId = $element->siteId;
+            return $result[MetaTitleContainer::CONTAINER_TYPE];
+        });
+        // Add metaTagContainer
+        $seomaticField->addStringField('metaTagContainer')->resolve(function (Element $element) {
+            // $root contains the data returned by the field below
+            $result = ContainerHelper::getContainerArrays(
+                [MetaTagContainer::CONTAINER_TYPE],
+                $element->uri,
+                $element->siteId,
+                false
+            );
 
-        // Determine the siteId
-        if ($siteId === null) {
-            $siteId = Craft::$app->getSites()->currentSite->id
-                ?? Craft::$app->getSites()->primarySite->id
-                ?? 1;
-        }
-        $uri = trim($uri, '/');
+            return $result[MetaTagContainer::CONTAINER_TYPE];
+        });
+        // Add metaLinkContainer
+        $seomaticField->addStringField('metaLinkContainer')->resolve(function (Element $element) {
+            // $root contains the data returned by the field below
+            $result = ContainerHelper::getContainerArrays(
+                [MetaLinkContainer::CONTAINER_TYPE],
+                $element->uri,
+                $element->siteId,
+                false
+            );
 
-        // Add a caching layer on top of controller requests
-        $sourceId = '';
+            return $result[MetaLinkContainer::CONTAINER_TYPE];
+        });
+        // Add metaScriptContainer
+        $seomaticField->addStringField('metaScriptContainer')->resolve(function (Element $element) {
+            // $root contains the data returned by the field below
+            $result = ContainerHelper::getContainerArrays(
+                [MetaScriptContainer::CONTAINER_TYPE],
+                $element->uri,
+                $element->siteId,
+                false
+            );
+            Craft::dd($result);
 
-        list($sourceId, $sourceBundleType, $sourceHandle, $sourceSiteId)
-            = Seomatic::$plugin->metaBundles->getMetaSourceFromElement($element);
+            return $result[MetaScriptContainer::CONTAINER_TYPE];
+        });
+        // Add metaJsonLdContainer
+        $seomaticField->addStringField('metaJsonLdContainer')->resolve(function (Element $element) {
+            // $root contains the data returned by the field below
+            $result = ContainerHelper::getContainerArrays(
+                [MetaJsonLdContainer::CONTAINER_TYPE],
+                $element->uri,
+                $element->siteId,
+                false
+            );
 
-        $metaContainers = Seomatic::$plugin->metaContainers;
-        // Get our cache key
-        $asArrayKey = $asArray ? 'true' : 'false';
-        $cacheKey = $uri.$siteId.implode($containerKeys).$asArrayKey;
-        // Load the meta containers
-        $dependency = new TagDependency([
-            'tags' => [
-                $metaContainers::GLOBAL_METACONTAINER_CACHE_TAG,
-                $metaContainers::METACONTAINER_CACHE_TAG.$sourceId,
-                $metaContainers::METACONTAINER_CACHE_TAG.$uri.$siteId,
-                $metaContainers::METACONTAINER_CACHE_TAG.$cacheKey,
-            ],
-        ]);
+            return $result[MetaJsonLdContainer::CONTAINER_TYPE];
+        });
 
-        $cache = Craft::$app->getCache();
-        $result = $cache->getOrSet(
-            $this::CACHE_KEY.$cacheKey,
-            function () use ($uri, $siteId, $containerKeys, $asArray) {
-                $result = [];
-                Craft::info(
-                    'Meta controller container cache miss: '.$uri.'/'.$siteId,
-                    __METHOD__
-                );
-                // Load the meta containers and parse our globals
-                Seomatic::$plugin->metaContainers->previewMetaContainers($uri, $siteId, true);
-
-                // Iterate through the desired $containerKeys
-                foreach ($containerKeys as $containerKey) {
-                    if ($asArray) {
-                        $result[$containerKey] = Seomatic::$plugin->metaContainers->renderContainersArrayByType(
-                            $containerKey
-                        );
-                    } else {
-                        $result[$containerKey] = Seomatic::$plugin->metaContainers->renderContainersByType(
-                            $containerKey
-                        );
-                    }
+        $event->schema->addField('seomatic')
+            ->arguments(function (\markhuot\CraftQL\Builders\Field $field) {
+                $field->addIntArgument('siteId');
+                $field->addStringArgument('uri')->nonNull();
+            })
+            ->type($seomaticField)
+            ->resolve(function ($root, $args, $context, $info) {
+                // Return an entry that meets the passed in criteria.
+                $criteria = Entry::find()
+                    ->uri($args['uri']);
+                if (!empty($args['siteId'])) {
+                    $criteria->siteId($args['siteId']);
                 }
 
-                return $result;
-            },
-            Seomatic::$cacheDuration,
-            $dependency
-        );
-
-        return $result;
+                return $criteria->one();
+            });
     }
 }
