07e1291e7dfe1ef059c613e0586122b38d23d821
Andrew Welch <andrew@nystudio107.com>
2019-05-08 22:43:52 -0400

    Add SeoEvent for Solspace Calendar
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

---
+++ src/seomatic-config/eventmeta/Bundle.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * SEOmatic plugin for Craft CMS 3.x
+ *
+ * A turnkey SEO implementation for Craft CMS that is comprehensive, powerful,
+ * and flexible
+ *
+ * @link      https://nystudio107.com
+ * @copyright Copyright (c) 2019 nystudio107
+ */
+
+use nystudio107\seomatic\helpers\Config;
+use nystudio107\seomatic\seoelements\SeoEvent;
+
+/**
+ * @author    nystudio107
+ * @package   Seomatic
+ * @since     3.2.0
+ */
+
+return [
+    'bundleVersion'              => '1.0.0',
+    'sourceBundleType'           => SeoEvent::getMetaBundleType(),
+    'sourceId'                   => null,
+    'sourceName'                 => null,
+    'sourceHandle'               => null,
+    'sourceType'                 => 'event',
+    'sourceTemplate'             => '',
+    'sourceSiteId'               => null,
+    'sourceAltSiteSettings'      => [
+    ],
+    'sourceDateUpdated'          => new \DateTime(),
+    'metaGlobalVars'             => Config::getConfigFromFile('eventmeta/GlobalVars'),
+    'metaSiteVars'               => Config::getConfigFromFile('eventmeta/SiteVars'),
+    'metaSitemapVars'            => Config::getConfigFromFile('eventmeta/SitemapVars'),
+    'metaBundleSettings'         => Config::getConfigFromFile('eventmeta/BundleSettings'),
+    'metaContainers'             => Config::getMergedConfigFromFiles([
+        'eventmeta/TagContainer',
+        'eventmeta/LinkContainer',
+        'eventmeta/ScriptContainer',
+        'eventmeta/JsonLdContainer',
+        'eventmeta/TitleContainer',
+    ]),
+    'redirectsContainer'         => Config::getConfigFromFile('eventmeta/RedirectsContainer'),
+    'frontendTemplatesContainer' => Config::getConfigFromFile('eventmeta/FrontendTemplatesContainer'),
+];
---
+++ src/seomatic-config/eventmeta/GlobalVars.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * SEOmatic plugin for Craft CMS 3.x
+ *
+ * A turnkey SEO implementation for Craft CMS that is comprehensive, powerful,
+ * and flexible
+ *
+ * @link      https://nystudio107.com
+ * @copyright Copyright (c) 2019 nystudio107
+ */
+
+/**
+ * @author    nystudio107
+ * @package   Seomatic
+ * @since     3.2.0
+ */
+
+return [
+    '*' => [
+        'mainEntityOfPage'        => 'Event',
+        'seoTitle'                => '{event.title}',
+        'siteNamePosition'        => '',
+        'seoDescription'          => '',
+        'seoKeywords'             => '',
+        'seoImage'                => '',
+        'seoImageWidth'           => '',
+        'seoImageHeight'          => '',
+        'seoImageDescription'     => '',
+        'canonicalUrl'            => '{event.url}',
+        'robots'                  => 'all',
+        'ogType'                  => 'website',
+        'ogTitle'                 => '{seomatic.meta.seoTitle}',
+        'ogSiteNamePosition'      => '',
+        'ogDescription'           => '{seomatic.meta.seoDescription}',
+        'ogImage'                 => '{seomatic.meta.seoImage}',
+        'ogImageWidth'            => '{seomatic.meta.seoImageWidth}',
+        'ogImageHeight'           => '{seomatic.meta.seoImageHeight}',
+        'ogImageDescription'      => '{seomatic.meta.seoImageDescription}',
+        'twitterCard'             => 'summary_large_image',
+        'twitterCreator'          => '{seomatic.site.twitterHandle}',
+        'twitterTitle'            => '{seomatic.meta.seoTitle}',
+        'twitterSiteNamePosition' => '',
+        'twitterDescription'      => '{seomatic.meta.seoDescription}',
+        'twitterImage'            => '{seomatic.meta.seoImage}',
+        'twitterImageWidth'       => '{seomatic.meta.seoImageWidth}',
+        'twitterImageHeight'      => '{seomatic.meta.seoImageHeight}',
+        'twitterImageDescription' => '{seomatic.meta.seoImageDescription}',
+    ],
+];
