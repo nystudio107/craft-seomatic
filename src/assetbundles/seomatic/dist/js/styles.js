a2852d9f9cbe7f26756210d13a5d620cbd0fbbeb
Andrew Welch <andrew@nystudio107.com>
2019-05-22 11:52:01 -0400

    Fixed an issue where the dynamically populated schema menu would have improper padding in the `value`s
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/assetbundles/seomatic/dist/js/styles.js
+++ src/assetbundles/seomatic/dist/js/styles.js
@@ -2,10 +2,10 @@
  * @project        SEOmatic
  * @name           styles.js
  * @author         Andrew Welch
- * @build          Tue, May 21, 2019 4:27 PM ET
- * @release        ee945b719549145359093365d9e3d1757a19aeda [develop]
+ * @build          Wed, May 22, 2019 3:49 PM ET
+ * @release        1ca809b7c80903fbabb509fd8a27ba4778e3e5c5 [develop]
  * @copyright      Copyright (c) 2019 nystudio107
  *
  */
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{24:function(n,w,o){}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{41:function(n,w,o){}}]);
 //# sourceMappingURL=styles.js.map
