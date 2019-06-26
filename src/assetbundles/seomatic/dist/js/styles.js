ce5a3d614f77759eee0461034024f865481f1e29
Andrew Welch <andrew@nystudio107.com>
2019-06-26 08:50:02 -0400

    Updated assets build
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/assetbundles/seomatic/dist/js/styles.js
+++ src/assetbundles/seomatic/dist/js/styles.js
@@ -2,10 +2,10 @@
  * @project        SEOmatic
  * @name           styles.js
  * @author         Andrew Welch
- * @build          Thu, Jun 13, 2019 6:16 PM ET
- * @release        23b7de5f3c9b585eb06b41f0421807d3836f74e4 [develop]
+ * @build          Wed, Jun 26, 2019 12:48 PM ET
+ * @release        ac0d14219ea51f66d8ff4c795eb7653cdfe11010 [develop]
  * @copyright      Copyright (c) 2019 nystudio107
  *
  */
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{17:function(n,w,o){}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{16:function(n,w,o){}}]);
 //# sourceMappingURL=styles.js.map
