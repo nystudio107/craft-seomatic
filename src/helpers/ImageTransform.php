46b7e38278294f0ebae84af9f4a48c79964c8356
Andrew Welch <andrew@nystudio107.com>
2019-07-09 11:38:48 -0400

    Balance parens!
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/helpers/ImageTransform.php
+++ src/helpers/ImageTransform.php
@@ -111,7 +111,7 @@ class ImageTransform
                 $url = '';
             }
             // If we have a url, add an `mtime` param to cache bust
-            if (!empty($url) && empty(parse_url($url, PHP_URL_QUERY)))) {
+            if (!empty($url) && empty(parse_url($url, PHP_URL_QUERY))) {
                 $url = UrlHelper::url($url, [
                     'mtime' => $asset->dateModified->getTimestamp(),
                 ]);
