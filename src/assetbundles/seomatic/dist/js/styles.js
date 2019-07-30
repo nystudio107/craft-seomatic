1ecfa76cce10be72e0671e35c0388e7a0ee9e746
Andrew Welch <andrew@nystudio107.com>
2019-07-30 18:23:58 -0400

    rebuild assets
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/assetbundles/seomatic/dist/js/styles.js
+++ src/assetbundles/seomatic/dist/js/styles.js
@@ -2,20 +2,10 @@
  * @project        SEOmatic
  * @name           styles.js
  * @author         Andrew Welch
- * @build          Mon, Jul 29, 2019 10:19 PM ET
- * @release        6b56669ba84a6de53b567afcd274a19f6a49a6c8 [feature/refactor-content-seo-page]
+ * @build          Tue, Jul 30, 2019 9:51 PM ET
+ * @release        16302f137d84b18ff4cb897725055606c3a2e39b [feature/refactor-content-seo-page]
  * @copyright      Copyright (c) 2019 nystudio107
  *
  */
-
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[7],{
-
-/***/ 45:
-/***/ (function(module, exports, __webpack_require__) {
-
-// extracted by mini-css-extract-plugin
-
-/***/ })
-
-}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{47:function(n,w,o){}}]);
 //# sourceMappingURL=styles.js.map
