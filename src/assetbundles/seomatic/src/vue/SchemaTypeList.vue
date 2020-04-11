56c83411cbfe4f2accc48305b13dd1c4693ce0b3
Andrew Welch <andrew@nystudio107.com>
2020-04-11 10:42:33 -0400

    Working schema selector
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/assetbundles/seomatic/src/vue/SchemaTypeList.vue
+++ src/assetbundles/seomatic/src/vue/SchemaTypeList.vue
@@ -1,15 +1,25 @@
 <!-- Vue SFC -->
 <template>
-    <div>
-        <treeselect
-                v-model="value"
-                ref="treeselect"
-                name="schemaType"
-                :multiple="false"
-                :flat="false"
-                :default-expand-level="0"
-                :options="options"
-        />
+    <div class="field">
+        <div class="py-3">
+            <treeselect
+                    v-model="value"
+                    ref="treeselect"
+                    name="schemaType"
+                    :multiple="false"
+                    :flat="false"
+                    :default-expand-level="0"
+                    :options="options"
+            />
+        </div>
+        <div v-if="schemaName !== null" class="heading">
+            <div class="instructions">
+                <p>
+                    <a :href="'http://schema.org/' + schemaName" rel="noopener" target="_blank">{{ schemaName }} info: </a>
+                    {{ schemaDescription }}
+                </p>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -22,33 +32,48 @@
 
     Vue.use(VueAxios, axios);
 
-    let action = 'get-type-tree';
-    const api = Craft.getActionUrl('seomatic/json-ld/' + action);
     export default {
         // register the component
         components: { Treeselect },
         props: {
             entity: {type: String, default: null},
         },
+        watch: {
+            schemaName: function(value) {
+                let action = 'get-type';
+                const api = Craft.getActionUrl('seomatic/json-ld/' + action + '?schemaType=' + this.schemaName);
+                this.axios.get(api).then((response) => {
+                    if (response.data) {
+                        if (response.data) {
+                            this.schemaDescription = response.data.schemaTypeDescription;
+                        }
+                    }
+                });
+            }
+        },
         mounted() {
+            let action = 'get-type-tree';
+            const api = Craft.getActionUrl('seomatic/json-ld/' + action);
             this.axios.get(api).then((response) => {
                 if (response.data) {
                     this.options = response.data;
                     this.value = this.entity;
                 }
             });
-            console.log(this.$refs);
             this.$refs.treeselect.$on('input', (value, instance) => {
-                let collection = document.getElementsByClassName('vue-treeselect__input');
-                for (let item of collection) {
-                    //console.log(item);
-                    //item.value = value;
-                    $(document).trigger('schema-value-changed', value);
+                if (value === undefined) {
+                    this.schemaName = null;
+                } else  {
+                    let parts = value.split('.');
+                    this.schemaName = parts[parts.length - 1];
                 }
+                $(document).trigger('schema-value-changed', value);
             });
         },
         data() {
             return {
+                schemaName: null,
+                schemaDescription: null,
                 // define the default value
                 value: null,
                 // define options
