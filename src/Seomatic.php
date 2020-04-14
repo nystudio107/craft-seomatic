94528afdd57367be10e7ec0b09347916be5d3a0a
Andrew Welch <andrew@nystudio107.com>
2020-04-13 23:32:29 -0400

    Initial feedme support
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/Seomatic.php
+++ src/Seomatic.php
@@ -44,7 +44,6 @@ use craft\elements\User;
 use craft\elements\Entry;
 use craft\errors\SiteNotFoundException;
 use craft\events\ElementEvent;
-use craft\events\DeleteTemplateCachesEvent;
 use craft\events\PluginEvent;
 use craft\events\RegisterCacheOptionsEvent;
 use craft\events\RegisterComponentTypesEvent;
@@ -58,12 +57,15 @@ use craft\services\Elements;
 use craft\services\Fields;
 use craft\services\Gql;
 use craft\services\Plugins;
-use craft\services\TemplateCaches;
 use craft\services\UserPermissions;
 use craft\helpers\UrlHelper;
 use craft\utilities\ClearCaches;
 use craft\web\UrlManager;
 use craft\web\View;
+
+use craft\feedme\Plugin as FeedMe;
+use craft\feedme\events\RegisterFeedMeFieldsEvent;
+use craft\feedme\services\Fields as FeedMeFields;
 
 use markhuot\CraftQL\Builders\Schema;
 use markhuot\CraftQL\CraftQL;
@@ -607,6 +609,20 @@ class Seomatic extends Plugin
                 Schema::class,
                 AlterSchemaFields::EVENT,
                 [GetCraftQLSchema::class, 'handle']
+            );
+        }
+        // FeedMe Support
+        if (class_exists(FeedMe::class)) {
+            Event::on(
+                FeedMeFields::class,
+                FeedMeFields::EVENT_REGISTER_FEED_ME_FIELDS,
+                function(RegisterFeedMeFieldsEvent $e) {
+                    Craft::debug(
+                        'FeedMeFields::EVENT_REGISTER_FEED_ME_FIELDS',
+                        __METHOD__
+                    );
+                    $e->fields[] = DataType::class;
+                }
             );
         }
     }
