9cf061b4135e0de71ed102d467fc93491b7037a2
Andrew Welch <andrew@keluli.local>
2020-07-01 17:01:21 -0400

    Add testing framework

---
+++ src/tests/_support/Helper/Functional.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Woof plugin for Craft CMS 3.x
+ *
+ * Woof
+ *
+ * @link      https://nystudio107.com
+ * @copyright Copyright (c) 2020 nystudio107
+ */
+
+namespace Helper;
+
+use Codeception\Module;
+
+/**
+ * Class Functional
+ *
+ * Here you can define custom actions.
+ * All public methods declared in helper class will be available in $I
+ */
+class Functional extends Module
+{
+
+}
