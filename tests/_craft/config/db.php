cdaea58dc47e9ff54974453687747e87715fa45a
Andrew Welch <andrew@nystudio107.com>
2020-07-01 23:42:24 -0400

    Testing env
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- tests/_craft/config/db.php
+++ tests/_craft/config/db.php
@@ -1,12 +1,9 @@
 <?php
 
 return [
+    'dsn' => getenv('DB_DSN'),
     'password' => getenv('DB_PASSWORD'),
     'user' => getenv('DB_USER'),
-    'database' => getenv('DB_DATABASE'),
     'tablePrefix' => getenv('DB_TABLE_PREFIX'),
-    'driver' => getenv('DB_DRIVER'),
-    'port' => getenv('DB_PORT'),
     'schema' => getenv('DB_SCHEMA'),
-    'server' => getenv('DB_SERVER'),
 ];
