8471c077a77b678406948dad4c0eee2172fb61aa
Andrew Welch <andrew@nystudio107.com>
2020-08-15 21:59:01 -0400

    Remove stylelint & grid prefixing
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- postcss.config.js
+++ postcss.config.js
@@ -2,12 +2,11 @@ module.exports = {
     plugins: [
         require('postcss-import')({
             plugins: [
-                require('stylelint')
             ]
         }),
         require('tailwindcss')('./tailwind.config.js'),
         require('postcss-preset-env')({
-            autoprefixer: { grid: true },
+            autoprefixer: { grid: false },
             features: {
                 'nesting-rules': true
             }
