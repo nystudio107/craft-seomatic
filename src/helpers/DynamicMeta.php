f94150506fbe6efe77363367db081bf65b4c13f7
Andrew Welch <andrew@nystudio107.com>
2020-09-26 13:42:48 -0400

    Fixed an error that could occur if your primary site has no BaseUrl
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/helpers/DynamicMeta.php
+++ src/helpers/DynamicMeta.php
@@ -410,6 +410,7 @@ class DynamicMeta
         if (!empty(Seomatic::$settings->siteUrlOverride)) {
             $siteUrl = Seomatic::$settings->siteUrlOverride;
         }
+        $siteUrl = $siteUrl ?: '/';
         /** @var  $crumbs BreadcrumbList */
         $crumbs = Seomatic::$plugin->jsonLd->create([
             'type' => 'BreadcrumbList',
@@ -437,7 +438,7 @@ class DynamicMeta
             $crumbs->itemListElement[] = MetaJsonLd::create('ListItem', [
                 'position' => $position,
                 'name' => 'Homepage',
-                'item' => UrlHelper::stripQueryString($siteUrl),
+                'item' => UrlHelper::stripQueryString($siteUrl ?? '/'),
                 '@id' => $siteUrl,
             ]);
         }
