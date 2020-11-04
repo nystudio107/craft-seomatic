311fc4db5bd14247f6ab850d5899b515037ab6b3
John Doe <codemonkey@codemonkey.org>
2020-11-04 22:38:05 +0200

    Webpack all the things.

--- src/assetbundles/seomatic/dist/js/styles.js
+++ src/assetbundles/seomatic/dist/js/styles.js
@@ -2,8 +2,8 @@
  * @project        SEOmatic
  * @name           styles.js
  * @author         Andrew Welch
- * @build          Wed, Sep 30, 2020 1:31 PM ET
- * @release        e745ebada9d7cfc8369b483b9be24aeb89c766f9 [develop]
+ * @build          Wed, Nov 4, 2020 8:31 PM ET
+ * @release        f6cd03a3227a77c649e72c0970e5e1699eb1ad85 [feature/social-images]
  * @copyright      Copyright (c) 2020 nystudio107
  *
  */
