0656adb41118221e945ff2302a69fb4f58d8df07
Andrew Welch <andrew@nystudio107.com>
2021-02-17 12:31:31 -0500

    Move to seomatic.manifest.registerJsModules & seomatic.manifest.registerCssModules
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/variables/ManifestVariable.php
+++ src/variables/ManifestVariable.php
@@ -2,172 +2,73 @@
 
 namespace nystudio107\seomatic\variables;
 
-use nystudio107\seomatic\helpers\Manifest as ManifestHelper;
-use nystudio107\seomatic\assetbundles\seomatic\SeomaticAsset;
-
-use Craft;
 use craft\helpers\Template;
+use nystudio107\seomatic\helpers\Manifest as ManifestHelper;
+
+use Twig\Markup;
+use yii\base\InvalidConfigException;
+use yii\web\NotFoundHttpException;
 
 class ManifestVariable
 {
-    // Protected Static Properties
-    // =========================================================================
-
-    protected static $config = [
-        // If `devMode` is on, use webpack-dev-server to all for HMR (hot module reloading)
-        'useDevServer' => false,
-        // Manifest names
-        'manifest'     => [
-            'legacy' => 'manifest.json',
-            'modern' => 'manifest.json',
-        ],
-        // Public server config
-        'server'       => [
-            'manifestPath' => '/',
-            'publicPath' => '/',
-        ],
-        // webpack-dev-server config
-        'devServer'    => [
-            'manifestPath' => 'http://127.0.0.1:8080',
-            'publicPath' => '/',
-        ],
-    ];
-
     // Public Methods
     // =========================================================================
 
     /**
-     * ManifestVariable constructor.
+     * Get the passed in JS modules from the manifest, and register them in the current Craft view
      *
-     * @param array $config
+     * @param array $modules
+     * @throws InvalidConfigException
+     * @throws NotFoundHttpException
      */
-    public function __construct($config = [])
+    public function registerJsModules(array $modules)
     {
-        ManifestHelper::invalidateCaches();
-        $bundle = new SeomaticAsset();
-        $baseAssetsUrl = Craft::$app->assetManager->getPublishedUrl(
-            $bundle->sourcePath,
-            true
-        );
-        self::$config['server']['manifestPath'] = Craft::getAlias($bundle->sourcePath);
-        self::$config['server']['publicPath'] = $baseAssetsUrl;
-        $useDevServer = getenv('NYS_PLUGIN_DEVSERVER');
-        if ($useDevServer !== false) {
-            self::$config['useDevServer'] = (bool)$useDevServer;
-        }
+        ManifestHelper::registerJsModules($modules);
     }
 
     /**
+     * Get the passed in CS modules from the manifest, and register them in the current Craft view
+     *
+     * @param array $modules
+     * @throws InvalidConfigException
+     * @throws NotFoundHttpException
+     */
+    public function registerCssModules(array $modules)
+    {
+        ManifestHelper::registerCssModules($modules);
+    }
+
+    /**
+     * Get the passed in JS module from the manifest, then output a `<script src="">` tag for it in the HTML
+     *
      * @param string     $moduleName
      * @param bool       $async
      * @param null|array $config
      *
-     * @return \Twig\Markup
-     * @throws \yii\web\NotFoundHttpException
+     * @return null|Markup
+     * @throws NotFoundHttpException
      */
-    public function includeCssModule(string $moduleName, bool $async = false, $config = null): \Twig\Markup
+    public function includeJsModule(string $moduleName, bool $async = false)
     {
         return Template::raw(
-            ManifestHelper::getCssModuleTags(self::$config, $moduleName, $async) ?? ''
+            ManifestHelper::includeJsModule($moduleName, $async) ?? ''
         );
     }
 
     /**
-     * Returns the CSS file in $path wrapped in <style></style> tags
+     * Get the passed in CS module from the manifest, then output a `<link>` tag for it in the HTML
      *
-     * @param string $path
-     *
-     * @return \Twig\Markup
-     */
-    public function includeInlineCssTags(string $path): \Twig\Markup
-    {
-        return Template::raw(
-            ManifestHelper::getCssInlineTags($path) ?? ''
-        );
-    }
-
-    /**
-     * Returns the uglified loadCSS rel=preload Polyfill as per:
-     * https://github.com/filamentgroup/loadCSS#how-to-use-loadcss-recommended-example
-     *
-     * @return \Twig\Markup
-     */
-    public static function includeCssRelPreloadPolyfill(): \Twig\Markup
-    {
-        return Template::raw(
-            ManifestHelper::getCssRelPreloadPolyfill() ?? ''
-        );
-    }
-
-    /**
      * @param string     $moduleName
      * @param bool       $async
      * @param null|array $config
      *
-     * @return null|\Twig\Markup
-     * @throws \yii\web\NotFoundHttpException
+     * @return Markup
+     * @throws NotFoundHttpException
      */
-    public function includeJsModule(string $moduleName, bool $async = false, $config = null)
+    public function includeCssModule(string $moduleName, bool $async = false): Markup
     {
         return Template::raw(
-            ManifestHelper::getJsModuleTags(self::$config, $moduleName, $async) ?? ''
-        );
-    }
-
-    /**
-     * Return the URI to a module
-     *
-     * @param string $moduleName
-     * @param string $type
-     * @param null   $config
-     *
-     * @return null|string
-     * @throws \yii\web\NotFoundHttpException
-     */
-    public function getModuleUri(string $moduleName, string $type = 'modern', $config = null)
-    {
-        return ManifestHelper::getModule(self::$config, $moduleName, $type, true);
-    }
-
-    /**
-     * Include the Safari 10.1 nomodule fix JavaScript
-     *
-     * @return \Twig\Markup
-     */
-    public function includeSafariNomoduleFix(): \Twig\Markup
-    {
-        return Template::raw(
-            ManifestHelper::getSafariNomoduleFix() ?? ''
-        );
-    }
-
-    /**
-     * Returns the contents of a file from a URI path
-     *
-     * @param string $path
-     *
-     * @return \Twig\Markup
-     */
-    public function includeFile(string $path): \Twig\Markup
-    {
-        return Template::raw(
-            ManifestHelper::getFile($path) ?? ''
-        );
-    }
-
-    /**
-     * Returns the contents of a file from the $fileName in the manifest
-     *
-     * @param string $fileName
-     * @param string $type
-     * @param null   $config
-     *
-     * @return \Twig\Markup
-     */
-    public function includeFileFromManifest(string $fileName, string $type = 'legacy', $config = null): \Twig\Markup
-    {
-        return Template::raw(
-            ManifestHelper::getFileFromManifest($config, $fileName, $type) ?? ''
+            ManifestHelper::includeCssModule($moduleName, $async) ?? ''
         );
     }
 }
