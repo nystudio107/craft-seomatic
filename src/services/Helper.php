c12570b53764543ed08424d5289f54bf41b93409
Hector D. Byrd <hector@ambrosia.net>
2021-08-12 08:34:24 +0300

    Fix a bug when a collection did not exist.

--- src/services/Helper.php
+++ src/services/Helper.php
@@ -527,8 +527,10 @@ class Helper extends Component
         if (in_array($collectionName, ['metaGlobalVars', 'metaSitemapVars'], true)) {
             foreach ($inheritedValues as $bundle) {
                 /** @var $bundle MetaBundle */
-                if (is_bool($bundle->{$collectionName}[$settingName]) || !empty($bundle->{$collectionName}[$settingName])) {
-                    return $bundle;
+                if (isset($bundle->{$collectionName}[$settingName])) {
+                    if (is_bool($bundle->{$collectionName}[$settingName]) || !empty($bundle->{$collectionName}[$settingName])) {
+                        return $bundle;
+                    }
                 }
             }
         }
