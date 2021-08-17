a183412f1c9b785118d2bf651b3dd24070a43f72
Andrew Welch <andrew@keluli.local>
2021-08-17 16:23:40 -0400

    Updated assets build

--- src/assetbundles/seomatic/dist/js/javascript-editor.js
+++ src/assetbundles/seomatic/dist/js/javascript-editor.js
@@ -2,9 +2,9 @@
  * @project        seomatic
  * @name           javascript-editor.js
  * @author         Andrew Welch
- * @build          Tue Aug 17 2021 17:32:38 GMT+0000 (Coordinated Universal Time)
+ * @build          Tue Aug 17 2021 20:22:32 GMT+0000 (Coordinated Universal Time)
  * @copyright      Copyright (c) 2021 ©2020 nystudio107.com
  *
  */
-(self.webpackChunkseomatic=self.webpackChunkseomatic||[]).push([[339],{9156:function(e,t,s){var i=s(8616);s(4579),s(440),$((function(){$("textarea.seomatic-javascript-editor").each((function(){var e=$(this),t=$("<div>",{position:"absolute",width:"98%",height:700,class:e.attr("class")}).insertBefore(e);e.css("display","none");var s=i.edit(t[0]);s.renderer.setShowGutter(e.data("gutter")),s.getSession().setValue(e.val()),s.getSession().setMode("ace/mode/javascript"),s.setTheme("ace/theme/github"),s.setFontSize(14),s.setShowPrintMargin(!1),s.setDisplayIndentGuides(!0),s.renderer.setShowGutter(!0),s.setHighlightActiveLine(!1),s.session.setUseWorker(!1),s.setOptions({minLines:10,maxLines:1/0}),e.closest("form").submit((function(){e.val(s.getSession().getValue())}))}))}))}},function(e){"use strict";e.O(0,[216],(function(){return t=9156,e(e.s=t);var t}));e.O()}]);
+(self.webpackChunkseomatic=self.webpackChunkseomatic||[]).push([[339],{9156:function(e,t,s){var i=s(8616);s(4579),s(440),$((function(){$("textarea.seomatic-javascript-editor").each((function(){var e=$(this),t=$("<div>",{position:"absolute",width:"98%",height:700,class:e.attr("class")}).insertBefore(e);e.css("display","none");var s=i.edit(t[0]);s.renderer.setShowGutter(e.data("gutter")),s.getSession().setValue(e.val()),s.getSession().setMode("ace/mode/javascript"),s.setTheme("ace/theme/github"),s.setFontSize(14),s.setShowPrintMargin(!1),s.setDisplayIndentGuides(!0),s.renderer.setShowGutter(!0),s.setHighlightActiveLine(!1),s.session.setUseWorker(!1),s.setOptions({minLines:10,maxLines:1/0}),e.closest("form").submit((function(){e.val(s.getSession().getValue())}))}))}))}},function(e){e.O(0,[216],(function(){return t=9156,e(e.s=t);var t}));e.O()}]);
 //# sourceMappingURL=javascript-editor.js.map
--- src/assetbundles/seomatic/dist/js/twig-editor.js
+++ src/assetbundles/seomatic/dist/js/twig-editor.js
@@ -2,9 +2,9 @@
  * @project        seomatic
  * @name           twig-editor.js
  * @author         Andrew Welch
- * @build          Tue Aug 17 2021 17:32:38 GMT+0000 (Coordinated Universal Time)
+ * @build          Tue Aug 17 2021 20:22:32 GMT+0000 (Coordinated Universal Time)
  * @copyright      Copyright (c) 2021 ©2020 nystudio107.com
  *
  */
-(self.webpackChunkseomatic=self.webpackChunkseomatic||[]).push([[840],{7357:function(e,t,s){var i=s(8616);s(1773),s(440),$((function(){$("textarea.seomatic-twig-editor").each((function(){var e=$(this),t=$("<div>",{position:"absolute",width:"98%",height:700,class:e.attr("class")}).insertBefore(e);e.css("display","none");var s=i.edit(t[0]);s.renderer.setShowGutter(e.data("gutter")),s.getSession().setValue(e.val()),s.getSession().setMode("ace/mode/twig"),s.setTheme("ace/theme/github"),s.setFontSize(14),s.setShowPrintMargin(!1),s.setDisplayIndentGuides(!0),s.renderer.setShowGutter(!0),s.setHighlightActiveLine(!1),s.setOptions({minLines:10,maxLines:1/0}),e.closest("form").submit((function(){e.val(s.getSession().getValue())}))}))}))}},function(e){"use strict";e.O(0,[216],(function(){return t=7357,e(e.s=t);var t}));e.O()}]);
+(self.webpackChunkseomatic=self.webpackChunkseomatic||[]).push([[840],{7357:function(e,t,s){var i=s(8616);s(1773),s(440),$((function(){$("textarea.seomatic-twig-editor").each((function(){var e=$(this),t=$("<div>",{position:"absolute",width:"98%",height:700,class:e.attr("class")}).insertBefore(e);e.css("display","none");var s=i.edit(t[0]);s.renderer.setShowGutter(e.data("gutter")),s.getSession().setValue(e.val()),s.getSession().setMode("ace/mode/twig"),s.setTheme("ace/theme/github"),s.setFontSize(14),s.setShowPrintMargin(!1),s.setDisplayIndentGuides(!0),s.renderer.setShowGutter(!0),s.setHighlightActiveLine(!1),s.setOptions({minLines:10,maxLines:1/0}),e.closest("form").submit((function(){e.val(s.getSession().getValue())}))}))}))}},function(e){e.O(0,[216],(function(){return t=7357,e(e.s=t);var t}));e.O()}]);
 //# sourceMappingURL=twig-editor.js.map
