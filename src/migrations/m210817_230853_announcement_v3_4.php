9ba6272802d8bf1412e35d8f2b393dc2e43b4753
Andrew Welch <andrew@nystudio107.com>
2021-08-17 21:18:17 -0400

    Check for Craft v3.7 before running migration, improve wording
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/migrations/m210817_230853_announcement_v3_4.php
+++ src/migrations/m210817_230853_announcement_v3_4.php
@@ -15,17 +15,19 @@ class m210817_230853_announcement_v3_4 extends Migration
      */
     public function safeUp()
     {
-        Craft::$app->announcements->push(
-            function($language) {
-                return Craft::t('seomatic', 'SEOmatic - SEO Settings fields', [], $language);
-            },
-            function($language) {
-                return Craft::t('seomatic', 'The [SEO Settings]({url}) fields in SEOmatic now feature **Override** light switches next to each setting, to improve the content authoring experience.', [
-                    'url' => 'https://nystudio107.com/docs/seomatic/fields.html',
-                ], $language);
-            },
-            'seomatic'
-        );
+        if (version_compare(Craft::$app->getVersion(), '3.7', '>=')) {
+            Craft::$app->announcements->push(
+                function ($language) {
+                    return Craft::t('seomatic', 'SEO Settings fields', [], $language);
+                },
+                function ($language) {
+                    return Craft::t('seomatic', 'The [SEO Settings]({url}) fields now feature **Override** light switches next to each setting, letting you explicitly override SEO settings on a per-entry basis.', [
+                        'url' => 'https://nystudio107.com/docs/seomatic/fields.html',
+                    ], $language);
+                },
+                'seomatic'
+            );
+        }
     }
 
     /**
