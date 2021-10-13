cd1b488da1df0cdd56e174a957574b15853dd37f
Andrew Welch <andrew@keluli.local>
2021-10-13 13:15:40 -0400

    Ensure URLs with no `path` work in `decomposeUrl()` ([#1001](https://github.com/nystudio107/craft-seomatic/issues/1001))

--- src/helpers/UrlHelper.php
+++ src/helpers/UrlHelper.php
@@ -161,7 +161,7 @@ class UrlHelper extends CraftUrlHelper
         if (filter_var($pathOrUrl, FILTER_VALIDATE_URL)) {
             $url_parts = parse_url($pathOrUrl);
             $result['prefix'] = $url_parts['scheme'] . '://' . $url_parts['host'];
-            $result['path'] = $url_parts['path'];
+            $result['path'] = $url_parts['path'] ?? '';
             $result['suffix'] = '';
             $result['suffix'] .= empty($url_parts['query']) ? '' : '?' . $url_parts['query'];
             $result['suffix'] .= empty($url_parts['fragment']) ? '' : '#' . $url_parts['fragment'];
