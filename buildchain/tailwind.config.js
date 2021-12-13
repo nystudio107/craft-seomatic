18cb3cd48c4cf8277983260f36914cbcee3d58ad
Andrew Welch <andrew@keluli.local>
2021-12-13 15:00:11 -0500

    feat: Update to TailwindCSS ^3.0.0

--- buildchain/tailwind.config.js
+++ buildchain/tailwind.config.js
@@ -1,24 +1,21 @@
 // module exports
 module.exports = {
-  mode: 'jit',
-  purge: {
-    content: [
-      '../src/templates/**/*.{twig,html}',
-      '../src/assetbundles/seomatic/src/**/*.{vue,html,js}',
+  content: [
+    '../src/templates/**/*.{twig,html}',
+    '../src/assetbundles/seomatic/src/**/*.{vue,html,js}',
+  ],
+  layers: [
+    'base',
+    'components',
+    'utilities',
+  ],
+  mode: 'layers',
+  options: {
+    whitelist: [
+      '../src/assetbundles/seomatic/src/css/components/*.css',
+      './node_modules/tokenfield/dist/tokenfield.css',
+      './node_modules/@riophae/vue-treeselect/dist/vue-treeselect.css'
     ],
-    layers: [
-      'base',
-      'components',
-      'utilities',
-    ],
-    mode: 'layers',
-    options: {
-      whitelist: [
-        '../src/assetbundles/seomatic/src/css/components/*.css',
-        './node_modules/tokenfield/dist/tokenfield.css',
-        './node_modules/@riophae/vue-treeselect/dist/vue-treeselect.css'
-      ],
-    }
   },
   theme: {
   },
