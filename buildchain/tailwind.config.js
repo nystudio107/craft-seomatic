0210e4106c59343282cdd974da632df33b9f2aa1
Andrew Welch <andrew@nystudio107.com>
2021-12-22 16:10:15 -0500

    chore: Clean up Tailwind CSS config

--- buildchain/tailwind.config.js
+++ buildchain/tailwind.config.js
@@ -4,21 +4,7 @@ module.exports = {
     '../src/templates/**/*.{twig,html}',
     '../src/assetbundles/seomatic/src/**/*.{vue,html,js}',
   ],
-  layers: [
-    'base',
-    'components',
-    'utilities',
-  ],
-  mode: 'layers',
-  options: {
-    whitelist: [
-      '../src/assetbundles/seomatic/src/css/components/*.css',
-      './node_modules/tokenfield/dist/tokenfield.css',
-      './node_modules/@riophae/vue-treeselect/dist/vue-treeselect.css'
-    ],
-  },
   theme: {
   },
-  corePlugins: {},
   plugins: [],
 };
