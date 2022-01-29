8f37712381f08f5d11ef359b3d5b052af51c92a1
Andrew Welch <andrew@nystudio107.com>
2022-01-28 23:25:13 -0500

    chore: Updated assets build

--- src/assetbundles/seomatic/dist/js/seomatic.js
+++ src/assetbundles/seomatic/dist/js/seomatic.js
@@ -2,7 +2,7 @@
  * @project        seomatic
  * @name           seomatic.js
  * @author         Andrew Welch
- * @build          Wed Jan 12 2022 18:18:05 GMT+0000 (Coordinated Universal Time)
+ * @build          Sat Jan 29 2022 04:19:40 GMT+0000 (Coordinated Universal Time)
  * @copyright      Copyright (c) 2022 ©2020 nystudio107.com
  *
  */
