80d7782912990c0d478881c9ad4dd983a3781e54
Andrew Welch <andrew@keluli.local>
2022-03-11 18:16:21 -0500

    refactor: The `ads.txt` and `security.txt` templates are no longer enabled by default, because they require configuration before use ([#1077](https://github.com/nystudio107/craft-seomatic/issues/1077))

--- src/seomatic-config/globalmeta/FrontendTemplatesContainer.php
+++ src/seomatic-config/globalmeta/FrontendTemplatesContainer.php
@@ -9,8 +9,8 @@
  * @copyright Copyright (c) 2017 nystudio107
  */
 
-use nystudio107\seomatic\services\FrontendTemplates;
 use nystudio107\seomatic\models\FrontendTemplateContainer;
+use nystudio107\seomatic\services\FrontendTemplates;
 
 /**
  * @author    nystudio107
@@ -19,43 +19,47 @@ use nystudio107\seomatic\models\FrontendTemplateContainer;
  */
 
 return [
-    'name'        => 'Frontend Templates',
+    'name' => 'Frontend Templates',
     'description' => 'Templates that are rendered on the frontend',
-    'handle'      => FrontendTemplates::FRONTENDTEMPLATES_CONTAINER,
-    'class'       => (string)FrontendTemplateContainer::class,
-    'include'     => true,
-    'data'        => [
+    'handle' => FrontendTemplates::FRONTENDTEMPLATES_CONTAINER,
+    'class' => (string)FrontendTemplateContainer::class,
+    'include' => true,
+    'data' => [
         FrontendTemplates::HUMANS_TXT_HANDLE => [
-            'handle'          => FrontendTemplates::HUMANS_TXT_HANDLE,
-            'path'            => 'humans.txt',
-            'template'        => '_frontend/pages/humans.twig',
-            'controller'      => 'frontend-template',
-            'action'          => 'humans',
-            'templateString'  => '',
+            'include' => true,
+            'handle' => FrontendTemplates::HUMANS_TXT_HANDLE,
+            'path' => 'humans.txt',
+            'template' => '_frontend/pages/humans.twig',
+            'controller' => 'frontend-template',
+            'action' => 'humans',
+            'templateString' => '',
         ],
         FrontendTemplates::ROBOTS_TXT_HANDLE => [
-            'handle'          => FrontendTemplates::ROBOTS_TXT_HANDLE,
-            'path'            => 'robots.txt',
-            'template'        => '_frontend/pages/robots.twig',
-            'controller'      => 'frontend-template',
-            'action'          => 'robots',
-            'templateString'  => '',
+            'include' => true,
+            'handle' => FrontendTemplates::ROBOTS_TXT_HANDLE,
+            'path' => 'robots.txt',
+            'template' => '_frontend/pages/robots.twig',
+            'controller' => 'frontend-template',
+            'action' => 'robots',
+            'templateString' => '',
         ],
         FrontendTemplates::ADS_TXT_HANDLE => [
-            'handle'          => FrontendTemplates::ADS_TXT_HANDLE,
-            'path'            => 'ads.txt',
-            'template'        => '_frontend/pages/ads.twig',
-            'controller'      => 'frontend-template',
-            'action'          => 'ads',
-            'templateString'  => '',
+            'include' => false,
+            'handle' => FrontendTemplates::ADS_TXT_HANDLE,
+            'path' => 'ads.txt',
+            'template' => '_frontend/pages/ads.twig',
+            'controller' => 'frontend-template',
+            'action' => 'ads',
+            'templateString' => '',
         ],
         FrontendTemplates::SECURITY_TXT_HANDLE => [
-            'handle'          => FrontendTemplates::SECURITY_TXT_HANDLE,
-            'path'            => 'security.txt',
-            'template'        => '_frontend/pages/security.twig',
-            'controller'      => 'frontend-template',
-            'action'          => 'security',
-            'templateString'  => '',
+            'include' => false,
+            'handle' => FrontendTemplates::SECURITY_TXT_HANDLE,
+            'path' => 'security.txt',
+            'template' => '_frontend/pages/security.twig',
+            'controller' => 'frontend-template',
+            'action' => 'security',
+            'templateString' => '',
         ],
     ],
 ];
