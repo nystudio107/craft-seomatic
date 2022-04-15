e9d9fecbfda24767aaacffd6c31edac3961b6356
Andrew Welch <andrew@nystudio107.com>
2022-04-14 22:59:49 -0400

    refactor: Ensure that the `currentSite` is set to the requested one when processing headless requests via API endpoint or GraphQL, to ensure things like `siteUrl()` etc. resolve correctly ([#1111](https://github.com/nystudio107/craft-seomatic/issues/1111))

--- src/helpers/Container.php
+++ src/helpers/Container.php
@@ -11,12 +11,11 @@
 
 namespace nystudio107\seomatic\helpers;
 
-use nystudio107\seomatic\Seomatic;
-use nystudio107\seomatic\helpers\ImageTransform as ImageTransformHelper;
-
 use Craft;
 use craft\base\Element;
-
+use craft\errors\SiteNotFoundException;
+use nystudio107\seomatic\helpers\ImageTransform as ImageTransformHelper;
+use nystudio107\seomatic\Seomatic;
 use yii\caching\TagDependency;
 
 /**
@@ -37,19 +36,20 @@ class Container
     /**
      * Return an array of meta containers for the given array of keys
      *
-     * @param array    $containerKeys
-     * @param string   $uri
+     * @param array $containerKeys
+     * @param string $uri
      * @param int|null $siteId
-     * @param bool     $asArray
+     * @param bool $asArray
      *
      * @return array
      */
     public static function getContainerArrays(
-        array $containerKeys,
+        array  $containerKeys,
         string $uri,
-        int $siteId = null,
-        bool $asArray = false
-    ): array {
+        int    $siteId = null,
+        bool   $asArray = false
+    ): array
+    {
         // Normalize the incoming URI to account for `__home__`
         $uri = ($uri === '__home__') ? '' : $uri;
         // Determine the siteId
@@ -58,6 +58,16 @@ class Container
                 ?? Craft::$app->getSites()->primarySite->id
                 ?? 1;
         }
+        // Set Craft's current site to the siteId we decided upon, stashing the current site first
+        $sites = Craft::$app->getSites();
+        $oldCurrentSite = $siteId;
+        try {
+            $oldCurrentSite = $sites->getCurrentSite();
+        } catch (SiteNotFoundException $e) {
+            Craft::error($e->getMessage(), __METHOD__);
+        }
+        $sites->setCurrentSite($siteId);
+        // Trim the URI
         $uri = trim($uri, '/');
         // Add a caching layer on top of controller requests
         $sourceId = '';
@@ -71,24 +81,24 @@ class Container
         $metaContainers = Seomatic::$plugin->metaContainers;
         // Get our cache key
         $asArrayKey = $asArray ? 'true' : 'false';
-        $cacheKey = $uri.$siteId.implode($containerKeys).$asArrayKey.Seomatic::$environment;
+        $cacheKey = $uri . $siteId . implode($containerKeys) . $asArrayKey . Seomatic::$environment;
         // Load the meta containers
         $dependency = new TagDependency([
             'tags' => [
                 $metaContainers::GLOBAL_METACONTAINER_CACHE_TAG,
-                $metaContainers::METACONTAINER_CACHE_TAG.$sourceId.$sourceBundleType.$siteId,
-                $metaContainers::METACONTAINER_CACHE_TAG.$uri.$siteId,
-                $metaContainers::METACONTAINER_CACHE_TAG.$cacheKey,
+                $metaContainers::METACONTAINER_CACHE_TAG . $sourceId . $sourceBundleType . $siteId,
+                $metaContainers::METACONTAINER_CACHE_TAG . $uri . $siteId,
+                $metaContainers::METACONTAINER_CACHE_TAG . $cacheKey,
             ],
         ]);
 
         $cache = Craft::$app->getCache();
         $result = $cache->getOrSet(
-            self::CACHE_KEY.$cacheKey,
+            self::CACHE_KEY . $cacheKey,
             function () use ($uri, $siteId, $containerKeys, $asArray) {
                 $result = [];
                 Craft::info(
-                    'Meta controller container cache miss: '.$uri.'/'.$siteId,
+                    'Meta controller container cache miss: ' . $uri . '/' . $siteId,
                     __METHOD__
                 );
                 // Load the meta containers and parse our globals
@@ -113,6 +123,8 @@ class Container
             Seomatic::$cacheDuration,
             $dependency
         );
+        // Restore old current site
+        $sites->setCurrentSite($oldCurrentSite);
         // Invalidate the cache we just created if there were pending image transforms in it
         if (ImageTransformHelper::$pendingImageTransforms) {
             TagDependency::invalidate($cache, $dependency->tags[3]);
