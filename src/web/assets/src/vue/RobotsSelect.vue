632278383e7f592ede406413f2c44923ea599d27
Andrew Welch <andrew@nystudio107.com>
2022-11-09 18:31:58 -0500

    refactor: WIP RobotsSelect

---
+++ src/web/assets/src/vue/RobotsSelect.vue
@@ -0,0 +1,53 @@
+<!-- Vue SFC -->
+<template>
+  <div class="field">
+    <div class="py-3">
+      <treeselect
+        ref="treeselect"
+        v-model="value"
+        :multiple="false"
+        :flat="true"
+        :default-expand-level="0"
+        :options="options"
+        :disabled="disabled"
+      />
+    </div>
+    <div
+      v-if="schemaName !== null"
+      class="heading"
+    >
+      <div class="instructions">
+        <p>
+          Robots
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Treeselect from '@riophae/vue-treeselect'
+import '@riophae/vue-treeselect/dist/vue-treeselect.css'
+
+export default {
+  // register the component
+  components: {Treeselect},
+  props: {
+    entity: {type: String, default: null},
+    disabled: {type: Boolean, default: false}
+  },
+  data() {
+    return {
+      // define the default value
+      value: null,
+      // define options
+      options: [],
+    }
+  },
+  mounted() {
+    this.$refs.treeselect.$on('input', (value, instance) => {
+      console.log(value);
+    });
+  },
+}
+</script>
