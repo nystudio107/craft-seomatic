ed942109eca2f42bbea3a1e5cafd92eecf68a8ce
Andrew Welch <andrew@nystudio107.com>
2023-01-31 21:00:47 -0500

    refactor: Fix templates path

--- buildchain/vite.config.ts
+++ buildchain/vite.config.ts
@@ -30,7 +30,7 @@ export default defineConfig(({command}) => ({
   plugins: [
     viteRestartPlugin({
       reload: [
-        './src/templates/**/*',
+        '../src/templates/**/*',
       ],
     }),
     createVuePlugin(),
