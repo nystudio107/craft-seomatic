1f59f3b4f808227288fd62dc7a93e75013fc3cbd
Andrew Welch <andrew@nystudio107.com>
2023-09-28 16:32:00 -0400

    feat: Initial Shopify Product implementation with a Faux ProductType model

---
+++ src/integrations/shopify/ProductType.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace nystudio107\seomatic\integrations\shopify;
+
+use Craft;
+use craft\base\Model;
+use craft\models\FieldLayout;
+use craft\models\Section_SiteSettings;
+use craft\shopify\Plugin as ShopifyPlugin;
+
+/**
+ * Faux Product type model since Shopify products have no concept of "sections"
+ *
+ * @method FieldLayout getFieldLayout()
+ *
+ * @author    Pixel & Tonic, Inc. <support@pixelandtonic.com>
+ * @copyright Copyright (c) 2017, Pixel & Tonic, Inc.
+ */
+class ProductType extends Model
+{
+    /**
+     * @var int ID
+     */
+    public $id = 31337;
+
+    /**
+     * @var string Name
+     */
+    public $name = 'Shopify Products';
+
+    /**
+     * @var string Handle
+     */
+    public $handle = 'shopifyproducts';
+
+    /**
+     * @var string SKU format
+     */
+    public $skuFormat;
+
+    /**
+     * @var string|null UID
+     */
+    public $uid = '31337product';
+
+
+    /**
+     * @inheritdoc
+     */
+    public function __toString()
+    {
+        return $this->handle;
+    }
+
+    /**
+     * Returns the product types's site-specific settings.
+     *
+     * @return ProductTypeSite[]
+     */
+    public function getSiteSettings(): array
+    {
+        $siteSettings = [];
+        $shopifyProducts = ShopifyPlugin::getInstance();
+        if ($shopifyProducts !== null) {
+            $shopifySettings = $shopifyProducts->getSettings();
+            $sites = Craft::$app->getSites()->getAllSites();
+            foreach ($sites as $site) {
+                $siteSettings[] = new Section_SiteSettings([
+                    'id' => $site->id,
+                    'sectionId' => $this->id,
+                    'siteId' => $site->id,
+                    'hasUrls' => empty($shopifySettings->uriFormat),
+                    'uriFormat' => $shopifySettings->uriFormat,
+                    'template' => $shopifySettings->template,
+                ]);
+            }
+        }
+
+        return $siteSettings;
+    }
+
+    /**
+     * @return FieldLayout
+     */
+    public function getProductFieldLayout(): FieldLayout
+    {
+        $shopifyProducts = ShopifyPlugin::getInstance();
+        if ($shopifyProducts !== null) {
+            return $shopifyProducts->getSettings()->getProductFieldLayout();
+        }
+    }
+}
---
+++ src/seomatic-config/shopifyproductmeta/JsonLdContainer.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * SEOmatic plugin for Craft CMS 3.x
+ *
+ * A turnkey SEO implementation for Craft CMS that is comprehensive, powerful,
+ * and flexible
+ *
+ * @link      https://nystudio107.com
+ * @copyright Copyright (c) 2017 nystudio107
+ */
+
+use nystudio107\seomatic\models\MetaJsonLdContainer;
+use nystudio107\seomatic\services\JsonLd as JsonLdService;
+
+/**
+ * @author    nystudio107
+ * @package   Seomatic
+ * @since     3.0.0
+ */
+
+return [
+    MetaJsonLdContainer::CONTAINER_TYPE . JsonLdService::GENERAL_HANDLE => [
+        'name' => 'General',
+        'description' => 'JsonLd Tags',
+        'handle' => JsonLdService::GENERAL_HANDLE,
+        'class' => (string)MetaJsonLdContainer::class,
+        'include' => true,
+        'dependencies' => [
+        ],
+        'data' => [
+            'mainEntityOfPage' => [
+                'type' => '{{ seomatic.meta.mainEntityOfPage }}',
+                'name' => '{{ seomatic.meta.seoTitle }}',
+                'headline' => '{{ seomatic.meta.seoTitle }}',
+                'description' => '{{ seomatic.meta.seoDescription }}',
+                'url' => '{{ seomatic.meta.canonicalUrl }}',
+                'mainEntityOfPage' => '{{ seomatic.meta.canonicalUrl }}',
+                'dateCreated' => '{{ product.dateCreated|atom }}',
+                'dateModified' => '{{ product.dateUpdated|atom }}',
+                'datePublished' => '{{ product.postDate|atom }}',
+                'copyrightYear' => '{{ product.postDate|date("Y") }}',
+                'inLanguage' => '{{ seomatic.meta.language }}',
+                'copyrightHolder' => [
+                    'id' => '{{ parseEnv(seomatic.site.identity.genericUrl) }}#identity',
+                ],
+                'author' => [
+                    'id' => '{{ parseEnv(seomatic.site.identity.genericUrl) }}#identity',
+                ],
+                'creator' => [
+                    'id' => '{{ parseEnv(seomatic.site.identity.genericUrl) }}#creator',
+                ],
+                'publisher' => [
+                    'id' => '{{ parseEnv(seomatic.site.identity.genericUrl) }}#creator',
+                ],
+                'image' => [
+                    'type' => 'ImageObject',
+                    'url' => '{{ seomatic.meta.seoImage }}',
+                ],
+                'potentialAction' => [
+                    'type' => 'SearchAction',
+                    'target' => '{{ seomatic.site.siteLinksSearchTarget }}',
+                    'query-input' => '{{ seomatic.helper.siteLinksQueryInput() }}',
+                ],
+                'sku' => '{{ product.getSku() }}',
+                'offers' => [
+                    'type' => 'Offer',
+                    'url' => '{{ seomatic.meta.canonicalUrl }}',
+                    'price' => '{{ product.getDefaultVariant().getSalePrice()|number_format(2, ".", "") }}',
+                    'priceCurrency' => '{{ craft.commerce.paymentCurrencies.primaryPaymentCurrencyIso() }}',
+                    'offeredBy' => [
+                        'id' => '{{ parseEnv(seomatic.site.identity.genericUrl) }}#identity',
+                    ],
+                    'seller' => [
+                        'id' => '{{ parseEnv(seomatic.site.identity.genericUrl) }}#identity',
+                    ],
+                    'availability' => 'http://schema.org/{% if product.getIsAvailable() %}InStock{% else %}OutOfStock{% endif % }}',
+                ],
+            ],
+        ],
+    ],
+];
