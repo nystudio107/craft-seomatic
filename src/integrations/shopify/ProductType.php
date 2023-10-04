e8ad5d03b60d44a8d9ff706a13a2ef7017eef1fd
Andrew Welch <andrew@nystudio107.com>
2023-10-04 13:06:22 +0200

    refactor: Fix comments

--- src/integrations/shopify/ProductType.php
+++ src/integrations/shopify/ProductType.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * SEOmatic plugin for Craft CMS 4.x
+ *
+ * A turnkey SEO implementation for Craft CMS that is comprehensive, powerful,
+ * and flexible
+ *
+ * @link      https://nystudio107.com
+ * @copyright Copyright (c) 2020 nystudio107
+ */
 
 namespace nystudio107\seomatic\integrations\shopify;
 
