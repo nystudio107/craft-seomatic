acc157020dbd1727074afc0704be250ded0f2903
Andrew Welch <andrew@nystudio107.com>
2023-10-04 15:06:46 +0200

    refactor: Increment bundle version

--- src/seomatic-config/shopifyproductmeta/Bundle.php
+++ src/seomatic-config/shopifyproductmeta/Bundle.php
@@ -19,7 +19,7 @@ use nystudio107\seomatic\seoelements\SeoShopifyProduct;
  */
 
 return [
-    'bundleVersion' => '1.0.37',
+    'bundleVersion' => '1.0.38',
     'sourceBundleType' => SeoShopifyProduct::getMetaBundleType(),
     'sourceId' => null,
     'sourceName' => null,
