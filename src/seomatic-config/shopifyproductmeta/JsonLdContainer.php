aac79c90b9ecb60bce3e6b7924e4363ad3306e1e
Andrew Welch <andrew@nystudio107.com>
2023-10-04 14:44:24 +0200

    refactor: No way to get the store's currency from the Shopify plugin currently

--- src/seomatic-config/shopifyproductmeta/JsonLdContainer.php
+++ src/seomatic-config/shopifyproductmeta/JsonLdContainer.php
@@ -66,6 +66,7 @@ return [
                     'type' => 'Offer',
                     'url' => '{{ seomatic.meta.canonicalUrl }}',
                     'price' => '{{ product.getDefaultVariant().price|number_format(2, ".", "") }}',
+                    // No way to get the store's currency from the Shopify plugin currently
                     'priceCurrency' => '',
                     'offeredBy' => [
                         'id' => '{{ parseEnv(seomatic.site.identity.genericUrl) }}#identity',
