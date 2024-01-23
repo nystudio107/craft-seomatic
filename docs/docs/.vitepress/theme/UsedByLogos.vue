1dfae17ab17058dc20ba38c7e764008faa03be14
Matt Stein <matt@adjacent.work>
2024-01-22 16:52:29 -0800

    docs: Improve landing
    - Use up-to-date Moz + Craft logos in SVG format.
    - Improve logo layout in docs.
    - Remove redundant language.
    - Use fenced code blocks in Installation section for more consistent format and easy copy+paste.
    - Edit intro to convey what it is before what it does.
    - Attempt to summarize core features.
    - Add alt text to images.
    - Lowercase control panel and bold click paths to follow Craft CMS documentation style.
    - Fix badge spacing in docs.
    - Use admonition for 404 redirect note.
    - Use title case for all headings.

---
+++ docs/docs/.vitepress/theme/UsedByLogos.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="used-by-logos">
+    <a href="https://moz.com/" target="_blank" rel="noopener">
+      <img src="./img/moz-logo.svg" aria-label="Moz logo" width="140" height="40" />
+    </a>
+    <a href="https://craftcms.com/" target="_blank" rel="noopener">
+      <img src="./img/craft-cms-logo.svg" aria-label="Craft CMS logo" width="220" height="55" />
+    </a>
+  </div>
+</template>
+
+<style scoped>
+.used-by-logos {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+}
+
+.used-by-logos a:not(:last-child) {
+  margin-right: 2rem;
+}
+</style>
