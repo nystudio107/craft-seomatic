da7d1c1bf411116cd244f4ba01a0ddb6ed29662a
Andrew Welch <andrew@nystudio107.com>
2024-01-23 23:46:46 -0500

    chore: Craft CMS 3.x -> Craft CMS

--- src/services/MetaBundles.php
+++ src/services/MetaBundles.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * SEOmatic plugin for Craft CMS 3.x
+ * SEOmatic plugin for Craft CMS
  *
  * A turnkey SEO implementation for Craft CMS that is comprehensive, powerful,
  * and flexible
