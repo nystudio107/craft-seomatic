cd5c30e745e5c4e5f2f205adfda76d13d9c5d4fc
Andrew Welch <andrew@nystudio107.com>
2024-02-22 18:48:09 -0500

    fix: Fixed an issue where clicking the SEOmatic CP nav item would result in a 403 exception if the current user didn't have permission to view the SEOmatic dashboard ([#1410](https://github.com/nystudio107/craft-seomatic/issues/1410))

--- src/Seomatic.php
+++ src/Seomatic.php
@@ -447,6 +447,14 @@ class Seomatic extends Plugin
                 'url' => 'seomatic/plugin',
             ];
         }
+        // SEOmatic doesn't really have an index page, so if the user can't access any sub nav items, we probably shouldn't show the main sub nav item either
+        if (empty($subNavs)) {
+            return null;
+        }
+        // A single sub nav item is redundant
+        if (count($subNavs) === 1) {
+            $subNavs = [];
+        }
         $navItem = array_merge($navItem, [
             'subnav' => $subNavs,
         ]);
@@ -943,7 +951,7 @@ class Seomatic extends Plugin
     {
         return [
             'seomatic' =>
-                'seomatic/settings/dashboard',
+                '',
             'seomatic/dashboard' =>
                 'seomatic/settings/dashboard',
             'seomatic/dashboard/<siteHandle:{handle}>' =>
