4ca00c60741c7ce05bee92815e87f01187ee7334
Andrew Welch <andrew@nystudio107.com>
2024-11-29 12:26:49 -0500

    chore: test

--- ecs.php
+++ ecs.php
@@ -3,6 +3,7 @@
 use craft\ecs\SetList;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
+#
 return static function(ECSConfig $ecsConfig): void {
     $ecsConfig->paths([
         __DIR__ . '/src',
