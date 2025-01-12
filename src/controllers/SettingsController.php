396097ad0e8f7b9a425496d1963229ccc0a19f25
Andrew Welch <andrew@nystudio107.com>
2025-01-11 22:02:23 -0500

    fix: Handle the case where SEOmatic settings for a specific `typeId` ended up in its own metabundle in addition to the default metabundle for that section ([#1557](https://github.com/nystudio107/craft-seomatic/issues/1557))

--- src/controllers/SettingsController.php
+++ src/controllers/SettingsController.php
@@ -532,11 +532,18 @@ class SettingsController extends Controller
         }
         $variables['typeMenu'] = $typeMenu;
         $variables['currentTypeId'] = null;
+        $variables['specificTypeId'] = null;
         if (!empty($typeMenu)) {
             $currentType = reset($typeMenu);
             $variables['currentType'] = $typeMenu[$typeId] ?? $currentType;
             $variables['currentTypeId'] = $typeId ?? key($typeMenu);
             $typeId = (int)$variables['currentTypeId'];
+        }
+        // If there's only one EntryType, don't bother displaying the menu
+        if (count($typeMenu) === 1) {
+            $variables['typeMenu'] = [];
+            $variables['specificTypeId'] = $typeId;
+            $typeId = null;
         }
         $pluginName = Seomatic::$settings->pluginName;
         // Asset bundle
@@ -648,6 +655,7 @@ class SettingsController extends Controller
         $sourceHandle = $request->getParam('sourceHandle');
         $siteId = $request->getParam('siteId');
         $typeId = $request->getParam('typeId') ?? null;
+        $specificTypeId = $request->getParam('specificTypeId') ?? null;
         $globalsSettings = $request->getParam('metaGlobalVars');
         $bundleSettings = $request->getParam('metaBundleSettings');
         $sitemapSettings = $request->getParam('metaSitemapVars');
@@ -686,6 +694,12 @@ class SettingsController extends Controller
             Seomatic::$plugin->metaBundles->syncBundleWithConfig($metaBundle, true);
             $metaBundle->typeId = $typeId;
             Seomatic::$plugin->metaBundles->updateMetaBundle($metaBundle, $siteId);
+            // If there's also a specific typeId associated with this section, save the same
+            // metabundle there too, to fix: https://github.com/nystudio107/craft-seomatic/issues/1557
+            if (!empty($specificTypeId)) {
+                $metaBundle->typeId = $specificTypeId;
+                Seomatic::$plugin->metaBundles->updateMetaBundle($metaBundle, $siteId);
+            }
 
             Seomatic::$plugin->clearAllCaches();
             Craft::$app->getSession()->setNotice(Craft::t('seomatic', 'SEOmatic content settings saved.'));
