b9d1b92d1213294f91d50b31e9c3a2f00075ae56
Andrew Welch <andrew@nystudio107.com>
2025-01-14 13:37:04 -0500

    fix: Handle sections with only 1 entry type properly ([#1557](https://github.com/nystudio107/craft-seomatic/issues/1557))

--- src/controllers/SettingsController.php
+++ src/controllers/SettingsController.php
@@ -528,7 +528,7 @@ class SettingsController extends Controller
         $variables['typeMenu'] = $typeMenu;
         $variables['currentTypeId'] = null;
         $variables['specificTypeId'] = null;
-        if (!empty($typeMenu)) {
+        if (count($typeMenu) > 1) {
             $currentType = reset($typeMenu);
             $variables['currentType'] = $typeMenu[$typeId] ?? $currentType;
             $variables['currentTypeId'] = $typeId ?? key($typeMenu);
@@ -537,8 +537,7 @@ class SettingsController extends Controller
         // If there's only one EntryType, don't bother displaying the menu
         if (count($typeMenu) === 1) {
             $variables['typeMenu'] = [];
-            $variables['specificTypeId'] = $typeId;
-            $typeId = null;
+            $variables['specificTypeId'] = $typeId ?? key($typeMenu);
         }
         $pluginName = Seomatic::$settings->pluginName;
         // Asset bundle
