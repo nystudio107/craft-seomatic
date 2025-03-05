8a430f573576faa64ff5cacbefb18fc028dc62c7
Andrew Welch <andrew@nystudio107.com>
2025-03-05 15:50:19 -0500

    fix: Fixed an issue where Commerce Product types didn't show the language menu in Content SEO ([#1577](https://github.com/nystudio107/craft-seomatic/issues/1577))

--- src/controllers/SettingsController.php
+++ src/controllers/SettingsController.php
@@ -10,10 +10,12 @@
 namespace nystudio107\seomatic\controllers;
 
 use Craft;
+use craft\commerce\models\ProductType;
 use craft\elements\Asset;
 use craft\errors\MissingComponentException;
 use craft\helpers\Cp;
 use craft\helpers\UrlHelper;
+use craft\models\Section;
 use craft\models\Site;
 use craft\web\Controller;
 use craft\web\UrlManager;
@@ -21,6 +23,7 @@ use DateTime;
 use Illuminate\Support\Collection;
 use nystudio107\seomatic\assetbundles\seomatic\SeomaticAsset;
 use nystudio107\seomatic\autocompletes\TrackingVarsAutocomplete;
+use nystudio107\seomatic\base\SeoElementInterface;
 use nystudio107\seomatic\helpers\ArrayHelper;
 use nystudio107\seomatic\helpers\DynamicMeta as DynamicMetaHelper;
 use nystudio107\seomatic\helpers\Field as FieldHelper;
@@ -562,7 +565,7 @@ class SettingsController extends Controller
         );
         // Enabled sites
         $this->setMultiSiteVariables($siteHandle, $siteId, $variables);
-        $this->cullDisabledSites($sourceBundleType, $sourceHandle, $variables);
+        $this->cullDisabledSites($seoElement, $sourceBundleType, $sourceHandle, $variables);
         // Meta Bundle settings
         Seomatic::$previewingMetaContainers = true;
         // Get the site to copy the settings from, if any
@@ -1327,17 +1330,18 @@ class SettingsController extends Controller
      * Remove any sites for which meta bundles do not exist (they may be
      * disabled for this section)
      *
+     * @param class-string<SeoElementInterface> $seoElement
      * @param string $sourceBundleType
      * @param string $sourceHandle
      * @param array $variables
      */
-    protected function cullDisabledSites(string $sourceBundleType, string $sourceHandle, array &$variables)
+    protected function cullDisabledSites($seoElement, string $sourceBundleType, string $sourceHandle, array &$variables)
     {
-        $entries = Craft::$app->getEntries();
-        $section = $entries->getSectionByHandle($sourceHandle);
+        /** @var Section|ProductType $section */
+        $section = $seoElement::sourceModelFromHandle($sourceHandle);
         $sectionSiteIds = [];
         if ($section) {
-            $sectionSettings = $entries->getSectionSiteSettings($section->id);
+            $sectionSettings = $section->getSiteSettings();
             foreach ($sectionSettings as $sectionSetting) {
                 $sectionSiteIds[] = $sectionSetting->siteId;
             }
