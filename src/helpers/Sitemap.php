2c04634341d54c3cde6cdd1f198d2b584c7444bf
Andrew Welch <andrew@nystudio107.com>
2025-04-15 15:53:48 -0400

    refactor: Encode sitemap entities to make sure they follow the RFC-3986 standard for URIs, the RFC-3987 standard for IRIs and the XML standard. ref: https://sitemaps.org/protocol.html#escaping

--- src/helpers/Sitemap.php
+++ src/helpers/Sitemap.php
@@ -271,7 +271,7 @@ class Sitemap
                     $lines[] = '<url>';
                     // Standard sitemap key/values
                     $lines[] = '<loc>';
-                    $lines[] = Html::encode($url);
+                    $lines[] = self::encodeSitemapEntity($url);
                     $lines[] = '</loc>';
                     $lines[] = '<lastmod>';
                     $lines[] = $dateUpdated->format(DateTime::W3C);
@@ -321,12 +321,12 @@ class Sitemap
                                             if ($primarySiteId === $altSourceSiteId && Seomatic::$settings->addXDefaultHrefLang) {
                                                 $lines[] = '<xhtml:link rel="alternate"'
                                                     . ' hreflang="x-default"'
-                                                    . ' href="' . Html::encode($altUrl) . '"'
+                                                    . ' href="' . self::encodeSitemapEntity($altUrl) . '"'
                                                     . ' />';
                                             }
                                             $lines[] = '<xhtml:link rel="alternate"'
                                                 . ' hreflang="' . $altSiteSettings['language'] . '"'
-                                                . ' href="' . Html::encode($altUrl) . '"'
+                                                . ' href="' . self::encodeSitemapEntity($altUrl) . '"'
                                                 . ' />';
                                         }
                                     }
@@ -453,6 +453,19 @@ class Sitemap
         $lines[] = '</urlset>';
 
         return implode('', $lines);
+    }
+
+    /**
+     * Encode sitemap entities to make sure they follow the RFC-3986 standard for URIs, the RFC-3987 standard for IRIs
+     * and the XML standard.
+     * ref: https://sitemaps.org/protocol.html#escaping
+     *
+     * @param string $text
+     * @return string
+     */
+    public static function encodeSitemapEntity(string $text): string
+    {
+        return Html::encode(UrlHelper::encodeUrl($text));
     }
 
     /**
@@ -589,7 +602,7 @@ class Sitemap
                     $transform = Craft::$app->getImageTransforms()->getTransformByHandle($metaBundle->metaSitemapVars->sitemapAssetTransform ?? '');
                     $lines[] = '<image:image>';
                     $lines[] = '<image:loc>';
-                    $lines[] = Html::encode(UrlHelper::absoluteUrlWithProtocol($asset->getUrl($transform, true)));
+                    $lines[] = self::encodeSitemapEntity(UrlHelper::absoluteUrlWithProtocol($asset->getUrl($transform, true)));
                     $lines[] = '</image:loc>';
                     // Handle the dynamic field => property mappings
                     foreach ($metaBundle->metaSitemapVars->sitemapImageFieldMap as $row) {
@@ -597,7 +610,7 @@ class Sitemap
                         $propName = $row['property'] ?? '';
                         if (!empty($fieldName) && !empty($asset[$fieldName]) && !empty($propName)) {
                             $lines[] = '<image:' . $propName . '>';
-                            $lines[] = Html::encode($asset[$fieldName]);
+                            $lines[] = self::encodeSitemapEntity($asset[$fieldName]);
                             $lines[] = '</image:' . $propName . '>';
                         }
                     }
@@ -607,7 +620,7 @@ class Sitemap
                 case 'video':
                     $lines[] = '<video:video>';
                     $lines[] = '<video:content_loc>';
-                    $lines[] = Html::encode(UrlHelper::absoluteUrlWithProtocol($asset->getUrl()));
+                    $lines[] = self::encodeSitemapEntity(UrlHelper::absoluteUrlWithProtocol($asset->getUrl()));
                     $lines[] = '</video:content_loc>';
                     // Handle the dynamic field => property mappings
                     foreach ($metaBundle->metaSitemapVars->sitemapVideoFieldMap as $row) {
@@ -615,7 +628,7 @@ class Sitemap
                         $propName = $row['property'] ?? '';
                         if (!empty($fieldName) && !empty($asset[$fieldName]) && !empty($propName)) {
                             $lines[] = '<video:' . $propName . '>';
-                            $lines[] = Html::encode($asset[$fieldName]);
+                            $lines[] = self::encodeSitemapEntity($asset[$fieldName]);
                             $lines[] = '</video:' . $propName . '>';
                         }
                     }
@@ -637,7 +650,7 @@ class Sitemap
                 $dateUpdated = $asset->dateUpdated ?? $asset->dateCreated ?? new DateTime();
                 $lines[] = '<url>';
                 $lines[] = '<loc>';
-                $lines[] = Html::encode(UrlHelper::absoluteUrlWithProtocol($asset->getUrl()));
+                $lines[] = self::encodeSitemapEntity(UrlHelper::absoluteUrlWithProtocol($asset->getUrl()));
                 $lines[] = '</loc>';
                 $lines[] = '<lastmod>';
                 $lines[] = $dateUpdated->format(DateTime::W3C);
