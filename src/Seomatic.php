5ae29f6b6405aff106cbf934f5fd0fea7227eb2e
Andrew Welch <andrew@nystudio107.com>
2026-04-29 15:10:49 -0400

    fix: Incomplete refactor

--- src/Seomatic.php
+++ src/Seomatic.php
@@ -20,7 +20,6 @@ use craft\elements\Entry;
 use craft\errors\SiteNotFoundException;
 use craft\events\DefineGqlTypeFieldsEvent;
 use craft\events\ElementEvent;
-use craft\events\ModelEvent;
 use craft\events\PluginEvent;
 use craft\events\RegisterCacheOptionsEvent;
 use craft\events\RegisterComponentTypesEvent;
@@ -543,17 +542,17 @@ class Seomatic extends Plugin
                 $event->types[] = Seomatic_MetaField::class;
             }
         );
-        // Handler: Elements::EVENT_AFTER_SAVE_ELEMENT
+        // Handler: Element::EVENT_AFTER_SAVE_ELEMENT
         Event::on(
             Elements::class,
             Elements::EVENT_AFTER_SAVE_ELEMENT,
-            static function(ModelEvent $event) {
+            static function(ElementEvent $event) {
                 Craft::debug(
                     'Elements::EVENT_AFTER_SAVE_ELEMENT',
                     __METHOD__
                 );
                 /** @var Element $element */
-                $element = $event->sender;
+                $element = $event->element;
                 self::$plugin->metaBundles->invalidateMetaBundleByElement(
                     $element,
                     $event->isNew
